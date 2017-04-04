24a47da177cccc2af678fd88754afd1163c46cb7
Vincent FUCHS <vincent.fuchs@gmail.com>
2017-04-04 23:09:06 +0530

    adding qpid profile for ApplicationTest, so that build is successful

--- complete/src/test/java/hello/ApplicationTest.java
+++ complete/src/test/java/hello/ApplicationTest.java
@@ -24,10 +24,12 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("qpid")
 public class ApplicationTest {
 
     @MockBean
