58630e7e0d9e2947211f03cf7b1d1e06608343c9
Gary Russell <grussell@pivotal.io>
2018-03-19 13:29:46 -0400

    Polishing - actually use the exchange/binding.
    The rabbit template was sending the message to the default exchange `""` instead
    of the configured topic exchange.
    
    Use a routing key.

--- complete/src/main/java/hello/Runner.java
+++ complete/src/main/java/hello/Runner.java
@@ -4,7 +4,6 @@ import java.util.concurrent.TimeUnit;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,21 +11,17 @@ public class Runner implements CommandLineRunner {
 
     private final RabbitTemplate rabbitTemplate;
     private final Receiver receiver;
-    private final ConfigurableApplicationContext context;
 
-    public Runner(Receiver receiver, RabbitTemplate rabbitTemplate,
-            ConfigurableApplicationContext context) {
+    public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
         this.receiver = receiver;
         this.rabbitTemplate = rabbitTemplate;
-        this.context = context;
     }
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Sending message...");
-        rabbitTemplate.convertAndSend(Application.queueName, "Hello from RabbitMQ!");
+        rabbitTemplate.convertAndSend(Application.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
         receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
-        context.close();
     }
 
 }
