c1c2168851049b7d85aaa53ef57be395b3879048
Jay Bryant <jbryant@pivotal.io>
2019-10-23 11:11:23 -0500

    Upgraded to Spring Boot 2.2
    I upgraded to Spring Boot 2.2.0, which necessitated changing the tests to work with JUnit 5 and replacing the Gradle wrappers.

--- complete/src/test/java/com/example/messagingrabbitmq/MessagingRabbitmqApplicationTest.java
+++ complete/src/test/java/com/example/messagingrabbitmq/MessagingRabbitmqApplicationTest.java
@@ -18,15 +18,13 @@ package com.example.messagingrabbitmq;
 
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class MessagingRabbitmqApplicationTest {
 
